070a990f65dd8a81767309f8adda869ee7885383
shitai246 <tai@a4p.me>
2014-10-30 20:36:17 +0900

    rebuild

--- build.sh
+++ build.sh
@@ -10,6 +10,13 @@ tar zcvf lockfile-1.0.tar.gz lockfile-1.0
 rpmbuild -ba ${THISDIR}/rpmbuild/SPECS/lockfile.spec
 unlink ~/rpmbuild
 
+
+rm -rf ${THISDIR}/centos
+mkdir -p ${THISDIR}/centos/6/os/{x86_64,SPRMS}
+
 cp ${THISDIR}/rpmbuild/RPMS/x86_64/lockfile-1.0-0.x86_64.rpm ${THISDIR}/centos/6/os/x86_64/lockfile-1.0-0.x86_64.rpm
 cp ${THISDIR}/rpmbuild/SRPMS/lockfile-1.0-0.src.rpm ${THISDIR}/centos/6/os/SPRMS/lockfile-1.0-0.src.rpm
+
+createrepo ${THISDIR}/centos/6/os/SPRMS/
+createrepo ${THISDIR}/centos/6/os/x86_64/
 
